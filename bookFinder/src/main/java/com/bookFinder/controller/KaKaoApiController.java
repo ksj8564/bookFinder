1fca4bcf5b73e321a8ab9bcb7bb29309637c4281
ksj <ksj871003@naver.com>
2018-07-11 11:57:20 +0900

    2차커밋

--- bookFinder/src/main/java/com/bookFinder/controller/KaKaoApiController.java
+++ bookFinder/src/main/java/com/bookFinder/controller/KaKaoApiController.java
@@ -1,14 +1,13 @@
 package com.bookFinder.controller;
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
  
 @RestController
 public class KaKaoApiController {
 
 	@GetMapping("/kakaoApi/v2/search/book")
-	public Object callKakaoRestApi(@PathVariable String apiUrl,HttpServletRequest request) {			
+	public Object callKakaoRestApi(HttpServletRequest request) {			
 		return "json";
 	} 
 }
